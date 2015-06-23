4b470fe7793d7c0e1cf0f95dcd31b37be8bfd33b
Christoph Wendt <chwend@microsoft.com>
2015-06-22 18:44:43 -0700

    Add demo activities (Crashes, Telemetry, Feedback)

--- app/src/main/java/net/hockeyapp/hockeyapp_sdk_android/ItemListActivity.java
+++ app/src/main/java/net/hockeyapp/hockeyapp_sdk_android/ItemListActivity.java
@@ -1,39 +1,116 @@
 package net.hockeyapp.hockeyapp_sdk_android;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+
+import net.hockeyapp.android.UpdateManager;
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.FeedbackManager;
 import net.hockeyapp.android.TelemetryManager;
 
-public class ItemListActivity extends ActionBarActivity {
+/**
+ * An activity representing a list of Items. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link ItemDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link ItemListFragment} and the item details
+ * (if present) is a {@link ItemDetailFragment}.
+ * <p>
+ * This activity also implements the required
+ * {@link ItemListFragment.Callbacks} interface
+ * to listen for item selections.
+ */
+public class ItemListActivity extends FragmentActivity
+        implements ItemListFragment.Callbacks {
+
+    public static final String APP_ID = "14b3e7126cb873e4df58ebf8a81ec903";
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
-    }
 
+        if (findViewById(R.id.item_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_item_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((ItemListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.item_list))
+                    .setActivateOnItemClick(true);
         }
+        UpdateManager.register(this, APP_ID);
+        FeedbackManager.register(this, APP_ID);
+        TelemetryManager.register(this.getApplicationContext(), this.getApplication(), APP_ID);
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkForCrashes();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterManagers();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterManagers();
+    }
+
+    private void checkForCrashes() {
+        CrashManager.register(this, APP_ID);
+    }
+
+    private void unregisterManagers() {
+        UpdateManager.unregister();
+        // unregister other managers if necessary...
+    }
+
+
+    /**
+     * Callback method from {@link ItemListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
+     */
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(ItemDetailFragment.ARG_ITEM_ID, id);
+            ItemDetailFragment fragment = new ItemDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.item_detail_container, fragment)
+                    .commit();
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, ItemDetailActivity.class);
+            detailIntent.putExtra(ItemDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
     }
 }
